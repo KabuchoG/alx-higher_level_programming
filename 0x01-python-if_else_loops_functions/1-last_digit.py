217336fcbac280034491930cc4160e8242cb5ca8
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-24 12:59:36 -0700

    new commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,16 +3,15 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0:
     number = str(number)
-    val = number[-1]
+    val = int(number[-1])
 else:
     number = str(number)
     neg = number[-1]
     val = int(neg) * -1
 print(f"Last digit of {number} is {val}", end = ' ')
-num = int(number[-1])
-if num > 5:
+if val > 5:
     print("and is greater than 5")
-elif num == 0:
+elif val == 0:
     print("and is zero")
 else:
     print("and is less than 6 and not zero")
