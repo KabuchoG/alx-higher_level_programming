3d9607ac5be4ddbc46c5eaf614e3579f26a97ad4
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-24 13:02:56 -0700

    new commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -12,6 +12,6 @@ print(f"Last digit of {number} is {val}", end = ' ')
 if val > 5:
     print("and is greater than 5")
 elif val == 0:
-    print("and is zero")
+    print("and is 0")
 else:
-    print("and is less than 6 and not zero")
+    print("and is less than 6 and not 0")
