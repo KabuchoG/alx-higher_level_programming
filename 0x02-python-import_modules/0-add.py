f2f99c0c97e3245d078f01d02b689ae36f7d6468
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-25 07:34:20 -0700

    new commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-import add_0
-def call():
-    if __name__ == '__main__':
-        a = 1
-        b = 2
-        print("{} + {} = {}".format(a, b, add_0.add(a, b)))
-
-
-
-call()
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
