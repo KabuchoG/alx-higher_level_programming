f65eb0b4d13609186166c413984793ac7fcc27ee
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-25 15:39:43 -0700

    new commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,22 +3,20 @@ if __name__ == "__main__":
     from calculator_1 import sub, mul, div, add
     import sys
     arg = sys.argv
-    if len(arg) < 4:
+    if len(arg) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = arg[1]
-    a = int(a)
+    a = int(arg[1])
     operator = arg[2]
-    c = arg[3]
-    c = int(c)
-    if operator != '+' or operator != '-' or operator != '*' or operator != '/':
+    b = int(arg[3])
+    if operator != '+' and operator != '-' and operator != '*' and operator != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     elif operator == '+':
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
     elif operator == '*':
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
-    elif operator == '/':
+    else:
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
