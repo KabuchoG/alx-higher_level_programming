a1ba6944632ed0791619b3f50f2243a3647f8347
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-25 07:22:40 -0700

    new comiit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import add_0
+def call():
+    if __name__ == '__main__':
+        a = 1
+        b = 2
+        print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+
+
+
+call()
