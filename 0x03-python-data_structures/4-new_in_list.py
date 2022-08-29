1ef5d0519d6d5ea1eca90fbdf8976632fcab3b33
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-29 07:08:33 -0700

    new commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,6 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return my_list.copy()
     else:
-        my_list.copy()
-        my_list[idx] = element
-        return my_list
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
