26e760d7ed3a900653ce68e3fcd42964a67a13d2
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-29 06:01:15 -0700

    new commit

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,6 @@ def replace_in_list(my_list, idx, element):
         return my_list
     if idx >= len(my_list):
         return my_list
-    my_list[idx] = element
+    else:
+        my_list[idx] = element
     return my_list
