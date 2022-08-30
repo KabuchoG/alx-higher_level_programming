382dc04dad60f2e56093f456c90e7607329fbfab
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-30 10:36:10 -0700

    new commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         new = matrix[i]
         for n in range(len(new)):
+            if n != 0:
+                print(' ', end='')
             print("{:d}".format(new[n]), end='')
         print()
