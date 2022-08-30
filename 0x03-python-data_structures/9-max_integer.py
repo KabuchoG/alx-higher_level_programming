345995e782f03b1bb8a0fe7dd2fc9f6f0a541a87
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-30 09:11:04 -0700

    new commit

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,4 +4,10 @@ def max_integer(my_list=[]):
         return None
     else:
         for i in range(len(my_list)):
-            if my_list[i] 
+            for n in range(len(my_list)):
+                if my_list[i] < my_list[n]:
+                    temp = my_list[i]
+                    my_list[i] = my_list[n]
+                    my_list[n] = temp
+        my_list.reverse()
+        return my_list[0]
