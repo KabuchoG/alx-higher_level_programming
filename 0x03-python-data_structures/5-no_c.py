2084c40469416818b4e79275db112e66af598d65
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-08-30 12:08:50 -0700

    new commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-     my_string.strip('Cc')
-    return my_string
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            if i == 0:
+                new = my_string[1:]
+            new = my_string[:i] + my_string[(i + 1):]
+    return new
