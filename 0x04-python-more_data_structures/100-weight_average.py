6c3ff01c1a4b67225dc0951f1dc76023a68ab425
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-01 14:40:53 -0700

    new commit

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not my_list:
+        return 0
     res = 0
     we = 0
     for i in my_list:
         res += + (i[0] * i[1])
         we += + i[1]
     return res / we
-
