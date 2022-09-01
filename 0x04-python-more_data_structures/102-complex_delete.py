f36145e006a0af406a6c1f3505d92c10a2e0a689
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-01 16:15:44 -0700

    new commit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    if value not in a_dictionary.items():
+def complex_delete(a_dictionary, value)
         return a_dictionary
     for i, j in a_dictionary.items():
         if j == value:
