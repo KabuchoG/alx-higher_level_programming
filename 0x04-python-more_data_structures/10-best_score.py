7c5be5ae3723a598fd30c94f6f3436e3b61afdef
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-01 06:35:07 -0700

    new commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    res = []
+    for i, j in a_dictionary.items():
+        res.append(j)
+    res.sort()
+    re = res.pop()
+    for i, j in a_dictionary.items():
+        if j == re:
+            return i
