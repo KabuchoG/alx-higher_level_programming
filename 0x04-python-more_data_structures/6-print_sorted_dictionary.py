4664277785da009f1113332eef2b476b37e94022
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-01 01:12:38 -0700

    new commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    res = {}
-    for i in a_dictionary:
-        res += i 
-    print("{:s}".format(sorted(res)))
+    print("{:s}".format(sorted(a-dictionary)))
