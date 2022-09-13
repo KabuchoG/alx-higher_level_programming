11eb06fd4737c245d0b69dee05f320930770f0d1
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-13 12:43:40 -0700

    new commit

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        a = arg[0]
-        b = arg[1]
-        res = fct(a, b)
+        res = fct(*args)
         return res
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
