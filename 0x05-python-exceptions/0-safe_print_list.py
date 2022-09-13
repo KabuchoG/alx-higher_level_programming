6771b6aedfbe2d7402c03ee449617c69f6ed4fc5
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-13 00:42:26 -0700

    new commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0;
+    num = 0
     try:
-        for n in  range(x):
+        for n in range(x):
             num += 1
             print(my_list[n], end='')
     except:
