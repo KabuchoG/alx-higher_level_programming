e76661b9e2dd7705abf4a253fa213eced9650588
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-13 06:29:06 -0700

    new commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     num = 0
-    for n in range (x):
+    for n in range(x):
         try:
             print("{:d}".format(my_list[n]), end='')
             num += + 1
         except (ValueError, TypeError):
             pass
-        #except IndexError:
-           #print()
+        """"except IndexError:
+        print()"""
     print()
     return num
