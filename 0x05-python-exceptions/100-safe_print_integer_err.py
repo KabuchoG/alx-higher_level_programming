9a914ddbfaddf9c8ac271c57b85a8be4c1908c8e
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-13 11:24:28 -0700

    new commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as err:
-        print(err)
+        print(err, file=sys.stderr)
         return False
