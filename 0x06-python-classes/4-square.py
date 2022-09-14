813c76582dcd62601aed969183d5ce088db23058
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-14 16:32:04 -0700

    new commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,20 +8,20 @@ class Square:
     setters and getters"""
     def __init__(self, size=0):
         """inititalizes each time an instance is created"""
-        if(type(size) is not int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
     @property
     def size(self):
         """This is a setter"""
         return self.__size
     @size.setter
     def size(self, value):
-        """Setter, sets a value"""
-        self.__size = value
+        """Setter, sets a value""" 
+        if(type(value) is not int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     def area(self):
         """Return the area"""
