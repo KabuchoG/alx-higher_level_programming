3c6aa5cd7559589500ee3077dc5d70e76709f948
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-14 15:51:07 -0700

    new commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,17 +8,9 @@ class Square:
     setters and getters"""
     def __init__(self, size=0):
         """inititalizes each time an instance is created"""
-        self.__size = size
-        @property
-        def retres(self):
-            """Returns the attribute"""
-            return self.__size
-        @retres.setter
-        def retres(self, size):
-            """Sets a value if certain conditions are met"""
-            if type(size) != "int":
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
+        if  (type(size) is not int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
