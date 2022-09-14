f454b6cd26f8d2b5d2cc4cb0e3bd0d1746b25e94
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-14 15:57:57 -0700

    new commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is a class that defines a private attribute"""
+
+
+class Square:
+    """Attributes self is used to reference an insance to itself.
+    size attribute is private and will fail toexcecute because there are no
+    setters and getters"""
+    def __init__(self, size=0):
+        """inititalizes each time an instance is created"""
+        if(type(size) is not int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+        def area(self):
+            return self.size ** 2
