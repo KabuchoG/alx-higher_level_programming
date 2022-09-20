b897af3cc7af071d82d8976119da729aa4673a4d
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-20 10:24:29 -0700

    new commmit

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Testing a function cases"""
+"""Testing a function cases
+"""
 import unittest
-from 6-max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 class TestMax(unittest.TestCase):
-    def testmax(self):
-        self.assertEqual(max_integer([1,3,5,7]), 7)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([2,2,2,2]), 2)
+    """
+    Testing class
+    """
+    def test_max_integer(self):
+        """
+        Testing method
+        """
+        a = [1,3,5,7]
+        b = []
+        c = [2,2,2,2,2]
+        self.assertEqual(max_integer(a), 7)
+        self.assertEqual(max_integer(b), None)
+        self.assertEqual(max_integer(c), 2)
 
