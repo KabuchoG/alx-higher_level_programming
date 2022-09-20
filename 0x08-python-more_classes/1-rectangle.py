bedde2a37460f4d6541df5549ec65a49eba4339c
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-20 16:09:24 -0700

    new commit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module defines a rectangle and its width
+"""
+
+
+class Rectangle:
+    """ A class with widh of a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        a reference to the instance itself
+        """
+
+        self.__width = width
+        self.__height = height
+        @property
+        def width(self):
+            """
+            get the width
+            """
+            return self.__width
+        @width.setter
+        def width(self, value):
+            """
+            setting a value
+            """
+
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            self.__value = value
+        @property
+        def height(self):
+            """
+            get the height
+            """
+            return self.__height
+        @height.setter
+        def height(self, value):
+            """
+            setting a value
+            """
+
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__value = value
