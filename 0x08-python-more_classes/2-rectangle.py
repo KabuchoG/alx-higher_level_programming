8fa39eae9cd2698604889c2c22531241c465a84c
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-09-21 01:13:13 -0700

    new commit

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -64,4 +64,4 @@ class Rectangle:
 
         if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__height*2) + (self.__width * 2)
+        return ((self.__height * 2) + (self.__width * 2))
