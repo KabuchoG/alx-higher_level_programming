a38c39a4e34430ab8493e9accaa2c2895681c196
George Kabucho <kabuchogeorge3@gmail.com>
2022-11-30 16:03:44 +0000

    fix semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@ const times = parseInt(process.argv[2]);
 if (times) {
   for (let i = 0; i < times; i++) {
     console.log('C is fun');
-  };
+  }
 } else {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 }
