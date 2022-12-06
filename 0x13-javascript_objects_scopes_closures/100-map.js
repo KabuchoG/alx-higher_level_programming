25e3d132cc7b38e9d199cf95310c620d4f0bef6c
KabuchoG <105174643+KabuchoG@users.noreply.github.com>
2022-12-06 12:46:15 +0000

    updates

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 
-const nl = list.map((m) => m * list.indexOf(m));
+const nl = list.map((m, idx) => m * idx);
 console.log(list);
 console.log(nl);
