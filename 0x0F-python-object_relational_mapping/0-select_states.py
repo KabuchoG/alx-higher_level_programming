4a2b731369035b1b7415fde399076e4a22444d99
George Kabucho <105174643+KabuchoG@users.noreply.github.com>
2022-12-09 10:05:05 +0000

    new co

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,13 @@ import MySQLdb
 
 def list_all():
     """ List all states"""
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
     host = "localhost"
     port = 3306
 
-    conn = MySQLdb.connect(host=host, port=port, user=user, passwd=password, db=db_name)
+    conn = MySQLdb.connect(host=host, user=username, passwd=password, db=db_name, port=port)
 
     curs = conn.cursor()
 
