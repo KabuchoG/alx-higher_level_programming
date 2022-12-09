90a6a925f75d8b2b6a8415d78b4cf9f981799961
George Kabucho <105174643+KabuchoG@users.noreply.github.com>
2022-12-09 11:36:24 +0000

    Update the file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,45 @@
 #!/usr/bin/python3
-""""lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa
-"""
+"""A module for a script that list all states starting with N"""
+
+
+def query():
+    """Lists all states starting with N from database hbtn_0e_0_usa"""
+
+    USER = argv[1]
+    PASS = argv[2]
+    DATABASE = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=USER,
+                         passwd=PASS,
+                         database=DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT id, name\
+            FROM states\
+            WHERE name LIKE BINARY 'N%'"
+    cursor.execute(sql)
+    query = cursor.fetchall()
+
+    my_list = []
+    for state in query:
+        my_list.append(state)
+
+    cursor.close()
+    db.close()
+
+    return my_list
+
+
+def main():
+    """print all states in my_list"""
+    my_list = query()
+    if my_list is not None:
+        for state in my_list:
+            print(state)
+
+
 if __name__ == "__main__":
     import MySQLdb
-    import sys
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    conn.close()
-    
+    from sys import argv
+    main()
