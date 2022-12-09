d50046dc4af73093dc4840276decb0b189500d1b
George Kabucho <105174643+KabuchoG@users.noreply.github.com>
2022-12-09 10:17:41 +0000

    news

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -1,10 +1,10 @@
 -- Create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS States (
+CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(2560) NOT NULL,
     PRIMARY KEY (id)
 );
-INSERT INTO States (name)
+INSERT INTO states (name)
 VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
