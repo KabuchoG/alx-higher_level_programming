b2f6ae1b2593e8808373dddc3ff04b63d6da9030
George Kabucho <105174643+KabuchoG@users.noreply.github.com>
2022-12-09 11:38:34 +0000

    Update the file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,45 +1,31 @@
 #!/usr/bin/python3
-"""A module for a script that list all states starting with N"""
+'''script for task 1'''
+
+import MySQLdb
+import sys
 
 
-def query():
-    """Lists all states starting with N from database hbtn_0e_0_usa"""
+def list_N():
+    '''lists all states with a name that starts with N'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = 3306
 
-    USER = argv[1]
-    PASS = argv[2]
-    DATABASE = argv[3]
-
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=USER,
-                         passwd=PASS,
-                         database=DATABASE)
-    cursor = db.cursor()
-    sql = "SELECT id, name\
-            FROM states\
-            WHERE name LIKE BINARY 'N%'"
-    cursor.execute(sql)
-    query = cursor.fetchall()
-
-    my_list = []
-    for state in query:
-        my_list.append(state)
-
-    cursor.close()
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
+                'ORDER BY states.id ASC')
+    result = cur.fetchall()
+    cur.close()
     db.close()
-
-    return my_list
-
-
-def main():
-    """print all states in my_list"""
-    my_list = query()
-    if my_list is not None:
-        for state in my_list:
-            print(state)
+    if result:
+        for row in result:
+            if row[1][0] == "N":
+                print(row)
 
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
-    main()
+    list_N()
