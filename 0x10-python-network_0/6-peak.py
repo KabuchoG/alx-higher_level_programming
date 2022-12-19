cc92448cd887c5f95f6d648b29b46d0193f06f05
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-19 11:51:40 -0800

    updates

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,5 +5,5 @@
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if list_of_integers:
-        list_of_integers.sort()
-        return list_of_integers[-1]
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
