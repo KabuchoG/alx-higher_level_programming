719a5df62d023511e7eeb58a595145fe452fc2fe
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 11:30:52 -0800

    update file

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""POST data"""
+
+if __name__ == "__main__":
+    from urllib import parse, request
+    from sys import argv
+
+    val = {"email": argv[2]}
+    data = parse.urlencode(val)
+    data = data.encode('ascii')
+    req = request.Request(argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+    print("Your email is: {}".format(body))
