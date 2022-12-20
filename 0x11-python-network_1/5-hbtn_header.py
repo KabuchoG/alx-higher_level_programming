ee2b7ef2fecfa6b674066b27eb18428d5b096d88
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 14:00:35 -0800

    update file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/biin/python3
+"""Get a header element"""
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    res = requests.get(argv[1])
+    response = res.headers
+    print(response.get('X-Request-Id'))
+    
