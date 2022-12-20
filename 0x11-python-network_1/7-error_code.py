1f993532090f501934bfc37d3d367dc0dbf7cb1e
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 14:18:53 -0800

    update file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""make use of command line arguments"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    try:
+        res = requests.get(argv[1])
+        print(res.text)
+    except requests.exceptions as e:
+        print("Error code:".format(res.status_code))
