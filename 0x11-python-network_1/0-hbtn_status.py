de7db0286165286ae6c9ddf84e8f45da9854abec
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 07:48:46 -0800

    update file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """Fetches a web resource"""
 
-from urllib import request
+if __name__ == "__main__":
 
-res = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(res) as response:
-    result = response.read()
-print(result)
+    from urllib import request
+
+    res = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(res) as response:
+        result = response.read()
+    print(result)
