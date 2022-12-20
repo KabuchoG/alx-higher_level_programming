e4607d416749e2f73055720283ec793f6cbdcd62
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 15:33:49 -0800

    update file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
 
     let = {}
     let['q'] = ""
-    if sys.argv[1]:
+    if len(sys.argv) == 2:
         let['q'] = sys.argv[1]
     res = requests.post('http://0.0.0.0:5000/search_user', data=let)
     try:
         response = res.json
-        if response:
-            print("[{}] {}".format(response.get('id'), response.get('name')))
-        else:
+        if response == {}:
             print("No result")
+        else:
+            print("[{}] {}".format(response.get('id'), response.get('name')))
     except requests.exceptions.JSONDecodeError:
         print("Not a valid JSON")
