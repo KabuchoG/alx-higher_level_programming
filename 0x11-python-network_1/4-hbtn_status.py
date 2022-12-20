3b20d8d3f037101c2e0036eac919222a67e94274
Kabucho"git config --global user.name Kabucho <kabuchogeorge3@gmail.com>
2022-12-20 13:52:22 -0800

    update file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     import requests
 
     res = requests.get('https://alx-intranet.hbtn.io/status')
+    res = res.text
     print("Body responses:")
     print("\t- type: {}".format(type(res)))
     print("\t- content: {}".format(res.content))
