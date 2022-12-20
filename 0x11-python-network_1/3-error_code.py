b0ead477673127c125c2e9a2475770c84e0f04e1
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 13:32:12 -0800

    update file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,11 +5,8 @@ if __name__ == "__main__":
     from urllib import parse, request, error
     from sys import argv
 
-    val = {"email": argv[2]}
-    data = parse.urlencode(val)
-    data = data.encode('ascii')
     try:
-        req = request.Request(argv[1], data)
+        req = request.Request(argv[1])
         with request.urlopen(req) as response:
             body = response.read()
         print(body.decode("utf-8"))
