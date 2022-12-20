483e8cc82bfa516258cbb4fbbc5f367aee79d75a
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 10:53:08 -0800

    update file

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,7 @@ if __name__ == "__main__":
     from urllib import request
     from sys import argv
 
-    headers = 'X-Request-Id'
+    head = 'X-Request-Id'
     fetched = request.Request(argv[1])
     with request.urlopen(fetched) as re:
-        res = dict(re).read()
-    print(res.get(headers))
+        print(dict(re.headers).get(head))
