1818bc429c4a254a2a71c249f643b68844bc43a3
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 15:52:43 -0800

    update file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
              print("[{}] {}".format(response.get('id'), response.get('name')))
         else:
            print("No result")
-    except requests.exceptions.JSONDecodeError:
+    except ValueError:
         print("Not a valid JSON")
