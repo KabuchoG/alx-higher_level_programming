e5a8269b5c4a8541c8b23bbbdf16c0cba044b10f
Kabucho G <kabuchogeorge3@gmail.com>
2022-12-20 16:11:49 -0800

    update file

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The first argument will be your username
+The second argument will be your password (in your case, a personal access token as password)
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    from requests.auth import HTTPBasicAuth
+
+    user = sys.argv[1]
+    passw = sys.argv[2]
+    cred = HTTPBasicAuth(user, passw)
+    res = requests.get('https://github.com/', auth=cred)
+    jr = res.json()
+    print(jr.get('id'))
